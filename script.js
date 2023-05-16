43ab0440dba56d506c1f9bf9546eafa566dafa64
Ayomide S.E.P <ayomidesunday31@gmail.com>
2023-05-16 16:45:19 +0100

    Fix eslint errors

--- script.js
+++ script.js
@@ -1,30 +1,26 @@
-let hamburger = document.querySelector(".hamburger-container");
-let mobileMenu = document.querySelector(".mobile-menu");
-let cancelMobile = document.querySelector(".cancel-mobile");
-let bgMobile = document.querySelector(".bg-mobile");
-const tags = document.querySelectorAll(".mobile-menu-link")
-
-hamburger.addEventListener("click", toggleMenu);
-cancelMobile.addEventListener("click", closeMenu);
-
-
-tags.forEach(tag => {
-    tag.addEventListener("click", () => {
-        closeMenu()
-    })
-})
+const hamburger = document.querySelector('.hamburger-container');
+const mobileMenu = document.querySelector('.mobile-menu');
+const cancelMobile = document.querySelector('.cancel-mobile');
+const bgMobile = document.querySelector('.bg-mobile');
+const tags = document.querySelectorAll('.mobile-menu-link');
 
 function toggleMenu() {
-    mobileMenu.classList.toggle("hide");
-    hamburger.classList.toggle("hide");
-    bgMobile.classList.toggle("hide");
-    bgMobile.classList.toggle("add-bg-on-mobile");
+  mobileMenu.classList.toggle('hide');
+  hamburger.classList.toggle('hide');
+  bgMobile.classList.toggle('hide');
+  bgMobile.classList.toggle('add-bg-on-mobile');
 }
-
 
 function closeMenu() {
-    mobileMenu.classList.toggle("hide");
-    hamburger.classList.toggle("hide");
-    bgMobile.classList.toggle("hide");
-    bgMobile.classList.toggle("add-bg-on-mobile");
+  mobileMenu.classList.toggle('hide');
+  hamburger.classList.toggle('hide');
+  bgMobile.classList.toggle('hide');
+  bgMobile.classList.toggle('add-bg-on-mobile');
 }
+hamburger.addEventListener('click', toggleMenu);
+cancelMobile.addEventListener('click', closeMenu);
+tags.forEach((tag) => {
+  tag.addEventListener('click', () => {
+    closeMenu();
+  });
+});
