4d3c453f0d6179cfce1aea8053dca67b16539c31
Shabi Adee <101591738+Shabiblac@users.noreply.github.com>
2023-05-18 22:21:58 +0100

    Add the form validation features

--- modal.js
+++ modal.js
@@ -174,3 +174,47 @@ buttons.forEach((btn) => {
     }
   });
 });
+
+const nameError = document.querySelector('#name-error');
+const emailError = document.querySelector('#email-error');
+const submitError = document.querySelector('#submit-error');
+
+function validate () {
+  const name = document.getElementById('user-name').value;
+
+  if(name.lenght == 0){
+    nameError.innerHTML = 'Name is required';
+    return false;
+  }
+  if(!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)){
+    nameError.innerHTML = 'Invalid name';
+    return false;
+  }
+  nameError.innerHTML = '<img src="images/checkbox-checked-solid-24 (1).png" alt="check icon">';
+  return true;
+}
+
+
+function validateEmail () {
+  const email = document.getElementById('user-email').value;
+
+  if(email.lenght === 0){
+    emailError.innerHTML = 'Email is required'
+    return false;
+  }
+  if(!email.match(/^([a-za-z0-9_\-\.])+\@([a-za-z0-9_\-\.])+\.([a-za-z]{2,4})$/)){
+    emailError.innerHTML = 'Invalid email'
+    return false;
+  }
+  emailError.innerHTML = '<img src="images/checkbox-checked-solid-24 (1).png" alt="check icon">';
+  return true;
+}
+
+function validateForm (){
+  if(!validate() || !validateEmail()){
+  submitError.style.display = 'block';
+    submitError.innerHTML = 'Please fix errors to submit';
+    setTimeout(function(){submitError.style.display = 'none';}, 3000)
+    return false;
+  }
+}
